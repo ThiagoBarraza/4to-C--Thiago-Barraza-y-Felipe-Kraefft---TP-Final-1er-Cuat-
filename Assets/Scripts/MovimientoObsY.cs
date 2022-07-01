0e94dfaafde46a60ea73a5c2a24e009ee1c93119
ThiagoBarraza <46645966@est.ort.edu.ar>
2022-07-01 04:07:18 -0300

    Added FlameTrowhers

---
+++ Assets/Scripts/MovimientoObsY.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoObsY : MonoBehaviour
+{
+    public Vector3 PosInicial;
+    public Vector3 PosFinal;
+
+    public float velocidad;
+    bool if1 = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (gameObject.transform.position.y >= PosFinal.y && !if1)
+        {
+            gameObject.transform.Translate(0 , -velocidad, 0);
+        }
+        else
+        {
+            if1 = true;
+        }
+        if (gameObject.transform.position.y != PosInicial.y && if1)
+        {
+            gameObject.transform.Translate(0, velocidad, 0);
+        }
+        else
+        {
+            if1 = false;
+        }
+    }
+}
--- Assets/Scripts/SpinX.cs
+++ Assets/Scripts/SpinX.cs
@@ -7,6 +7,7 @@ public class SpinX : MonoBehaviour
     Vector3 v3 = Vector3.zero;
     public float speed = 25.0f;
     public float Yrotation = 0f;
+    public float Zrotation = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,7 @@ public class SpinX : MonoBehaviour
     {
         v3.x += speed * Time.deltaTime;
         v3.y = Yrotation;
+        v3.z = Zrotation;
         transform.eulerAngles = v3;
     }
 }
